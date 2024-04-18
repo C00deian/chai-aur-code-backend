7bc9cf3f9cdf3dffc801bd3f98970043eaa15405
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-04-18 18:31:20 +0530

    need to check

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,21 +1,37 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
+const uplaod = multer({
 
-        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        // cb(null, file.fieldname + '-' + uniqueSuffix)
-        cb(null, file.originalname);
-    }
+    storage: multer.diskStorage({
+     
+        destination: function (req, file, cb) {
+            cb(null, './public/temp')
+        },
+        filename: function (req, file, cb) {
+
+            // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            // cb(null, file.fieldname + '-' + uniqueSuffix)
+            cb(null, file.originalname);
+        }
+    })
 });
 
-export const upload = multer(
-    {
-        storage
-        // storage: storage  ==  storage    Reason:- ES6 
-    }
+export { uplaod }
 
-)
+
+// const storage = multer.diskStorage({
+
+//     destination: function (req, file, cb) {
+//         cb(null, './public/temp')
+//     },
+//     filename: function (req, file, cb) {
+
+//         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//         // cb(null, file.fieldname + '-' + uniqueSuffix)
+//         cb(null, file.originalname);
+//     }
+
+
+// })
+
+//  export const  upload = multer({storage})
